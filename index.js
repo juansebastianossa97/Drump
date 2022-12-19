10dc34e15f2dc77c553ccbfdae417851252278b7
juansebastianossa97 <99688557+juansebastianossa97@users.noreply.github.com>
2022-12-19 11:37:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+
+// Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;   // .drum estamos cogiendo los elementos
+for (var i = 0; i < numberOfDrumButtons; i++){                         // de la clase .drum 
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    function handleClick(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttorAnimation(buttonInnerHTML);
+    }
+}
+
+// Detecting Keyboard
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttorAnimation(event.key);
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();                    
+            break;
+        case "l":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+function buttorAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+/*
+Version Larga
+document.querySelector("button").addEventListener("click", handleClick);
+function handleClick(){
+    alert("I got clicked!");
+}
+Version Simplificada
+document.querySelector("button").addEventListener("click", function() {alert("I got clicked!")});
+
+
+Ya lo usamos 
+
+
+*/
+
+
